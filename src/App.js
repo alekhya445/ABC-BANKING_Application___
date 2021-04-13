cb858c89cfc52191c996b4b9d7f45bda5c98f308
alekhya445 <69209236+alekhya445@users.noreply.github.com>
2021-04-13 18:31:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import'bootstrap/dist/js/bootstrap.bundle'
 import {BrowserRouter ,Route,Switch} from 'react-router-dom'
 import login from './Components/login'
-import Adminaxios from './Components/adminaxios'
-import Useraxios from './Components/useraxios'
+import Adminpage from './Components/adminpage'
+import Userpage from './Components/userpage'
 import React, { Component } from 'react'
 import signup from './Components/signup';
 import Adduser from './Components/adduser';
@@ -15,7 +15,7 @@ import Viewuser1 from './Components/viewuser1';
 import Edituser from './Components/edituser';
 import AddAmount from './Components/addamount';
 import CurrentBalence from './Components/currentadd';
-import MiniStatement from './Components/mini';
+import MiniStatement from './Components/ministatement';
 import CurrentBalance from './Components/currentdelamount';
 import Logout from './Components/logout';
 //import signup from './Components/signup';
@@ -40,16 +40,16 @@ import DelAmount from './Components/delamount';
         
         <Route path='/login' component={login}></Route>
         <Route path='/logout' component={Logout}></Route>
-        <Route path='/adminaxios' component={Adminaxios}></Route>
+        <Route path='/adminpage' component={Adminpage}></Route>
         <Route path='/viewuser/:id' component={Viewuser}></Route>
         <Route path='/viewuser1/:id' component={Viewuser1}></Route>
         <Route path='/edituser/:id' component={Edituser}></Route>
         <Route path='/adduser' component={Adduser}></Route>
-        <Route path='/useraxios' component={Useraxios}></Route>
+        <Route path='/userpage' component={Userpage}></Route>
         <Route path='/addamount' component={AddAmount}></Route>
         <Route path='/currentadd' component={CurrentBalence}></Route>
         <Route path='/currentdel' component={CurrentBalance}></Route>
-        <Route path='/mini/:id' component={MiniStatement}></Route>
+        <Route path='/ministatement/:id' component={MiniStatement}></Route>
         <Route path='/delamount' component={DelAmount}></Route>
         {/* <Route path='/viewpage' component={UserView}></Route> */}
         <Route path='/signup' component={signup}></Route>
